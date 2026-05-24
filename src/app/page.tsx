5257f61d59f94772a8689a9242ef5f43ea07e1bb
Tom Chauvet <tomchauvet@MacBook-Air-Tom.local>
2026-05-24 15:56:39 +0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-1 items-center justify-center p-8">
+      <h1 className="text-4xl font-semibold">Sales Club</h1>
+    </main>
+  );
+}
